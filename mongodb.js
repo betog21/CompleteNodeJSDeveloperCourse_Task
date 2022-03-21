6eda9e7e153e197652d08cd5690100a50f96f06f
Alberto González Balderas <albertoglezb@hotmail.com>
2022-03-21 10:30:22 -0600

    Examples to insert many documents were implemented.

--- mongodb.js
+++ mongodb.js
@@ -15,10 +15,48 @@ MongoClient.connect(
     }
 
     const db = client.db(dataBaseName);
-    db.collection("users").insertOne({
-      name: "Andrew",
-      age: 27,
-    });
-    console.log('Inserted.');
+    //Insert one example.
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Andrew",
+    //     age: 27,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.error("Error inserting user.", error);
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    //Insert many
+    // db.collection("users").insertMany(
+    //   [
+    //     { name: "Jen", age: 28 },
+    //     { name: "Gunther", age: 27 },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.error("Error inserting users.", error);
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    //Insert Many tasks.
+    db.collection("tasks").insertMany(
+      [
+        { description: "Task 1", completed: true },
+        { description: "Task 2", completed: true },
+        { description: "Task 3", completed: false },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.error("Unable to insert tasks.", error);
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );

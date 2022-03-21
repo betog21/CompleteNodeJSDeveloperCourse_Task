a8183200bbb0adbb8375110b9c45c65d15891513
Alberto González Balderas <albertoglezb@hotmail.com>
2022-03-21 17:16:37 -0600

    Validations were added to the user model.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,29 +1,51 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive numer");
+      }
+    },
+  },
+});
 
-// const me = new User({
-//     name: 'Andrew',
-//     age: 27
-// })
+const me = new User({
+  name: "    Andrew  ",
+  email: "mike@gmail.COm  ",
+});
 
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log("Error!", error);
+  });
 
 const Task = mongoose.model("Task", {
   description: {
@@ -34,16 +56,16 @@ const Task = mongoose.model("Task", {
   },
 });
 
-const task1 = new Task({
-  description: "Study Node",
-  completed: false,
-});
+// const task1 = new Task({
+//   description: "Study Node",
+//   completed: false,
+// });
 
-task1
-  .save()
-  .then(() => {
-    console.log("Task1 saved");
-  })
-  .catch((error) => {
-    console.error("Error: ", error);
-  });
+// task1
+//   .save()
+//   .then(() => {
+//     console.log("Task1 saved");
+//   })
+//   .catch((error) => {
+//     console.error("Error: ", error);
+//   });

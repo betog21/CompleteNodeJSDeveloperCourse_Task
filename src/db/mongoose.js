fa847caf108b1ede035a092afe23351d8b0c6281
Alberto González Balderas <albertoglezb@hotmail.com>
2022-03-21 16:55:06 -0600

    The task model was implemented.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,26 +1,49 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-})
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String
+//     },
+//     age: {
+//         type: Number
+//     }
+// })
 
-const me = new User({
-    name: 'Andrew',
-    age: 27
-})
+// const me = new User({
+//     name: 'Andrew',
+//     age: 27
+// })
 
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
+});
+
+const task1 = new Task({
+  description: "Study Node",
+  completed: false,
+});
+
+task1
+  .save()
+  .then(() => {
+    console.log("Task1 saved");
+  })
+  .catch((error) => {
+    console.error("Error: ", error);
+  });

421e3d2148ce7140cb04cebbb0c24a291abbb2b5
Alberto González Balderas <albertoglezb@hotmail.com>
2022-03-21 20:19:48 -0600

    The get task end points were created.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
 const Task = require("./models/task");
+const { ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,6 +22,30 @@ app.post("/users", (req, res) => {
     });
 });
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send();
+      }
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
   task
@@ -32,6 +57,16 @@ app.post("/tasks", (req, res) => {
       res.status(400).send(error);
     });
 });
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((error) => {});
+});
+
+app.get("/tasks/:id", (req, res) => {});
 
 app.listen(port, () => {
   console.log("Server is running on port: " + port);

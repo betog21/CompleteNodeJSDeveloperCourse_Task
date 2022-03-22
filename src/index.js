83a4eb9a2475911c861e4e28ee0be4c53bcb151c
Alberto González <alberto.gonzalez@distillery.com>
2022-03-22 14:16:01 -0600

    The delete user enpoint was added.

--- src/index.js
+++ src/index.js
@@ -72,6 +72,20 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    return res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   try {
     const task = new Task(req.body);

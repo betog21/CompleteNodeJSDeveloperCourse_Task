269b2d7c5aee63802ba37bd76704cfb0e2e334b5
Alberto González <alberto.gonzalez@distillery.com>
2022-03-22 13:28:53 -0600

    The tasks endpoints changed to usage async y await.

--- src/index.js
+++ src/index.js
@@ -42,40 +42,36 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
+app.post("/tasks", async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.status(202).send(tasks);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(202).send(tasks);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.get("/tasks/:id", (req, res) => {
-  Task.findById(req.params.id)
-    .then((task) => {
-      if (task) {
-        res.send(task);
-      } else {
-        res.status(404).send();
-      }
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (task) {
+      res.send(task);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 app.listen(port, () => {

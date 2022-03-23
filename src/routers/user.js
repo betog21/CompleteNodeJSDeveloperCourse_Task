e50ca6efdb8dae14d95ceb7714901d2cdee66232
Alberto González <alberto.gonzalez@distillery.com>
2022-03-23 11:04:49 -0600

    The get users was modified to get the user's profile with authentication.

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 
 router.post("/users", async (req, res) => {
   try {
@@ -27,13 +28,8 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+router.get("/users/me", auth, async (req, res) => {
+  return res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {

05101725e6863130fd37b2ff79b4bdf100d6b82b
Alberto González <alberto.gonzalez@distillery.com>
2022-03-24 11:54:08 -0600

    Tests were added to the update user endpoint.

--- tests/user.test.js
+++ tests/user.test.js
@@ -92,3 +92,38 @@ test("Should delete account for user", async () => {
 test("Should not delete account for unauthenticated user", async () => {
   await request(app).get("/users/me").send().expect(401);
 });
+
+test("Should upload avatar image", async () => {
+  await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar", "tests/fixtures/profile-pic.jpg")
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test("Should update valid user fields", async () => {
+  const newUserName = "Testing update";
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: newUserName,
+    })
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.name).toEqual(newUserName);
+});
+
+test("Should not update invalid user fields.", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      nationality: "None",
+    })
+    .expect(400);
+});

2f38689faeca7199de89366015d1e1d5e0f6b44c
Alberto González <alberto.gonzalez@distillery.com>
2022-03-24 10:35:28 -0600

    More tests were added to the Users endpoint.

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
+
+const userOne = {
+  name: "Kranky",
+  email: "kranky@kranky.com",
+  password: "PassKranky1010#",
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 test("Should signup a new user", async () => {
   await request(app)
@@ -10,4 +22,21 @@ test("Should signup a new user", async () => {
       password: "PassFranky1010#",
     })
     .expect(201);
+});
+
+test("Should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+});
+
+test("Shoul not login nonexistent user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({ email: userOne.email, password: "BadPassword" })
+    .expect(500);
 });
